b64ad4d842d8ef76ad4ec8c10942db578e5ae47b
Zachary Ning <nzy142625@gmail.com>
2019-09-20 03:31:52 +0900

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PBL2019_Robotics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// 设计器支持所需的方法 - 不要修改
+        /// コード エディターで変更しないでください。
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(338, 258);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 258);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(110, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Camera Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(128, 258);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(110, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Camera Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(338, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(444, 258);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(550, 258);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 287);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
+
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+//using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using BeautoRover;
+
+namespace PBL2019_Robotics
+{
+    public partial class Form1 : Form
+    {
+        // カメラを取得するオブジェクトを作る
+        // 创建视频捕获对象
+        private VideoCapture capture;
+        // ロボットを動くオブジェクトを作る
+        // 创建小车动作对象
+        BeautoRoverlib br = new BeautoRoverlib();
+        public Form1()
+        {
+            // 初期化
+            // 初始化
+            InitializeComponent();
+            // カメラ映像を取得する
+            // 获取摄像头影像
+            capture = VideoCapture.FromCamera(0);
+            // カメラデバイスが正常にオープンしたか確認
+            // 确认是否正常获取摄像头
+            //textBox1.Text = Convert.ToString(capture.IsOpened());
+        }
+
+        // スタートバートンを押す
+        // Start按钮按下事件
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            // 画面の高さを設定する
+            // 设定画面高度
+            capture.Set(CaptureProperty.FrameHeight, 240);
+            // 画面の幅を設定する
+            // 设定画面宽度
+            capture.Set(CaptureProperty.FrameWidth, 320);
+            // タイマー処理開始
+            // timer开始
+            timer1.Start();
+            // シリアルポートを開く
+            // 打开串口
+            //textBox1.Text = br.OpenCOMPort("COM4");
+            // スタートバートンを無効にする
+            // Start按钮无效化
+            buttonStart.Enabled = false;
+            // ストーブバートンを有効にする
+            // Stop按钮有效化
+            buttonStop.Enabled = true;
+        }
+
+        // ストーブバートンを押す
+        // Stop按钮按下事件
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            // タイマー処理停止
+            // timer停止
+            timer1.Stop();
+            // シリアルポートを閉じる
+            // 关闭串口
+            //textBox1.Text = br.Close();
+            // スタートバートンを有効にする
+            // Start按钮有效化
+            buttonStart.Enabled = true;
+            // ストーブバートンを無効にする
+            // Stop按钮无效化
+            buttonStop.Enabled = false;
+        }
+
+        // タイマー処理
+        // timer处理
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            // 映像をBGR空間からHSV空間に変更し，Matオブジェクトを作る
+            // 创建Mat对象，将摄像头影像从BGR空间转换到HSV空间
+            Mat img = capture.RetrieveMat().CvtColor(ColorConversionCodes.BGR2HSV);
+            // オブジェクトがnullかどうかを判断
+            // 对象null判断
+            if (img != null)
+            {
+                // 映像を表示する
+                // 显示摄像头影像
+                pictureBox1.Image = img.ToBitmap();
+                // メモリクリア
+                // 清理内存
+                /*if (GC.GetTotalMemory(false) > 600000)
+                {
+                    GC.Collect();
+                }*/
+                int sumPixelY = 0, sumPixelX = 0, count = 0;
+                int minX = 320, minY = 240, maxX = 0, maxY = 0;
+                for (int y = 0; y < img.Height; y++)
+                    for (int x = 0; x < img.Width; x++)
+                    {
+                        byte h = img.At<Vec3b>(y, x)[0];
+                        byte s = img.At<Vec3b>(y, x)[1];
+                        byte v = img.At<Vec3b>(y, x)[2];
+
+                        //if (r > 80 && b < 80 && g < 80)
+                        if ((h < 180 && h > 160) && (s > 80) && (v < 220 && v > 50))
+                        {
+                            img.Set<Vec3b>(y, x, new Vec3b(255, 255, 255));
+                            sumPixelY += y;
+                            sumPixelX += x;
+                            if (x < minX)
+                                minX = x;
+                            if (y < minY)
+                                minY = y;
+                            if (x > maxX)
+                                maxX = x;
+                            if (y > maxY)
+                                maxY = y;
+                            count++;
+                        }
+                        else
+                        {
+                            img.Set<Vec3b>(y, x, new Vec3b(0, 0, 0));
+                        }
+                        //textBox2.Text = Convert.ToString(capture.Get(CaptureProperty.Fps));
+                    }
+                Point center = new Point(sumPixelX / count, sumPixelY / count);
+                int r = ((maxX - minX) + (maxY - minY)) / 4;
+                img.Circle(center, r, Scalar.Green, 3);
+
+                textBox1.Text = Convert.ToString(img.At<Vec3b>(img.Height / 2, img.Width / 2)[0]);
+                textBox2.Text = Convert.ToString(img.At<Vec3b>(img.Height / 2, img.Width / 2)[1]);
+                textBox3.Text = Convert.ToString(img.At<Vec3b>(img.Height / 2, img.Width / 2)[2]);
+
+                // 処理した画像を表示
+                // 显示处理后影像
+                pictureBox2.Image = img.ToBitmap();
+            }
+            else
+            {
+                // オブジェクトがnullであればストーブ
+                // 对象为null停止timer
+                timer1.Stop();
+            }
+
+        }
+
+    }
+}
+
